e72a49cbb90abb1d60fd75bc313efe834ec8feb4
Marcelo <marcelojunin2010@hotmail.com>
2020-10-08 16:35:22 -0300

    creating clientDto

---
+++ src/main/java/br/com/digitalBank/dto/ClientDto.java
@@ -0,0 +1,29 @@
+package br.com.digitalBank.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ClientDto {
+
+    @NotNull(message = "Name may not be null")
+    private String name;
+
+    private String lastName;
+
+    @NotNull(message = "Email may not be null")
+    @Email(message = "Email format is invalid")
+
+    private String email;
+
+    @NotNull(message = "Cnh may not be null")
+    private String cnh;
+
+    private String dateOfBirth;
+
+    private String cpf;
+}
